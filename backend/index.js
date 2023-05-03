434d1741085357c724d75c8ae2f74cce843deed5
prashant1shukla <77752155+prashant1shukla@users.noreply.github.com>
2023-05-03 17:56:57 +0530

    learning express and api

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
 const express = require("express");
+const app=express();
+const mongoose=require
+app.use(express.json());
+
+app.listen(5000,()=>{
+    console.log("Server started!");
+})
+
+app.post("/post", async(req,res)=>{
+    console.log(req.body);
+    const {data}= req.body;
+
+    try{
+        if(data=="prashant"){
+            res.send({status:"ok"});
+        }
+        else{
+            res.send({status: "User not found"});
+        }
+    }
+    catch(error){
+        res.send({status:"Something went wrong, try again"});
+    }
+     
+});
+ 
